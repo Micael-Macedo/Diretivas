dfde19b2ba1457e0a3c8ae751efc47211b46e772
Micael-Macedo <micaelm2009@Hotmail.com>
2024-11-28 14:27:14 -0300

    feat: add switch component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwitchComponentComponent } from "./switch-component/switch-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SwitchComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/switch-component/switch-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch-component.component.html',
+  styleUrl: './switch-component.component.scss'
+})
+export class SwitchComponentComponent {
+  aba: string = '';
+
+}
